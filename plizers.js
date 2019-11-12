0887d207d96b8cef0e481e03e98dced363920a97
Izhaki <roey@gefri.org>
2019-11-12 22:10:20 +0000

    Add version bump and publishing plizers

--- plizers.js
+++ plizers.js
@@ -8,7 +8,20 @@ const list = async () => {
   await exe('ls -lh');
 };
 
+const bump = async semver => {
+  await exe(`npm version ${semver}`);
+  await exe(`git push`);
+  await exe(`git push --tags`);
+};
+
+const publish = async () => {
+  await exe('npm publish');
+  await exe('yarn release');
+};
+
 module.exports = {
   say,
   list,
+  bump,
+  publish,
 };
