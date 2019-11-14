f11901857a7d2bbe39617e28690d11818e8e1c5a
Izhaki <roey@gefri.org>
2019-11-14 00:37:41 +0000

    Reinstate yarn link in tests

--- test/hooks.js
+++ test/hooks.js
@@ -1,9 +1,9 @@
-// import execa from 'execa';
+import execa from 'execa';
 import { Before } from 'cucumber';
 import tempFolder from './utils/tempFolder';
 
 Before(async function() {
-  // await execa.command(`yarn link`);
+  await execa.command(`yarn link`);
   const { symLinkNodeModules, writeFile, exe } = tempFolder();
   symLinkNodeModules();
 
